13a8039584e033580d63c6d7740637a5963827d7
Juan <00jarg@gmail.com>
2016-12-06 02:38:52 -0300

    Add description

--- fish_greeting.fish
+++ fish_greeting.fish
@@ -1,4 +1,4 @@
-function fish_greeting
+function fish_greeting -d "Sage cow displaying a quote at random"
 	set -l QUOTE_FILE "$HOME/.local/share/quotes/chess.quotes"	
 	set -l QUOTE (head -n (math (random) "%" (cat $QUOTE_FILE | wc -l) "+1") $QUOTE_FILE | tail -1)
 	eval (shuf -n1 -e cowsay cowthink) -(shuf -n1 -e b d g p s t w y) "$QUOTE"
