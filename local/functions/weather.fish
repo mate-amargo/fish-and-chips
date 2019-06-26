31b9345e84984ef4d2c7c5c05d5410b17813ea46
Juan <00jarg@gmail.com>
2019-06-25 21:55:09 -0300

    Remove accuweather. Print last line of wttr.

--- local/functions/weather.fish
+++ local/functions/weather.fish
@@ -1,7 +1,3 @@
 function weather -d "Show weather from wttr.in"
-	if test (count $argv) -eq 1; and test $argv[1] = "-s"
-		curl -s 'http://www.accuweather.com/es/ar/bahia-blanca/2931/weather-forecast/2931?lang=es-ar'| awk -F\' '/acm_RecentLocationsCarousel.push\(\{name:"Bah/{print $1"\nTemperatura: "$10" °C\n" "Térmica: "$12" °C\n" $13}' | sed 's/.*"\(.*\)".*/\1/'
-	else
-		curl -s 'wttr.in/bahia_blanca'| head -n-3
-	end
+  curl -s 'wttr.in/bahia_blanca'| head -n-2
 end
