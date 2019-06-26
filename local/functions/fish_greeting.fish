798287c9ccff7e40bd3d86277a9c29de7381ab8d
Juan <00jarg@gmail.com>
2019-06-25 22:29:28 -0300

    Add chance to get fish_logo at greeting

--- local/functions/fish_greeting.fish
+++ local/functions/fish_greeting.fish
@@ -1,5 +1,9 @@
 function fish_greeting -d "Sage cow displaying a quote at random"
-	set -l QUOTE_FILE "$HOME/.local/share/quotes/chess.quotes"	
-	set -l QUOTE (head -n (math (random) "%" (cat $QUOTE_FILE | wc -l) "+1") $QUOTE_FILE | tail -1)
-	eval (shuf -n1 -e cowsay cowthink) -(shuf -n1 -e b d g p s t w y) "$QUOTE"
+  if test (shuf -n1 -e 0 1) -eq 0
+    set -l QUOTE_FILE "$HOME/.local/share/quotes/chess.quotes"	
+    set -l QUOTE (head -n (math (random) "%" (cat $QUOTE_FILE | wc -l) "+1") $QUOTE_FILE | tail -1)
+    eval (shuf -n1 -e cowsay cowthink) -(shuf -n1 -e b d g p s t w y) "$QUOTE"
+  else
+    fish_logo
+  end
 end
