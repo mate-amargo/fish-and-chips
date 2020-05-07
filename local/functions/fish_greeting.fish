82a754e143132519d27bac46daf6b9a244b1e195
Juan <00jarg@gmail.com>
2020-05-07 12:51:44 -0300

    Add archey3 and neofetch to greeting

--- local/functions/fish_greeting.fish
+++ local/functions/fish_greeting.fish
@@ -1,9 +1,14 @@
 function fish_greeting -d "Sage cow displaying a quote at random"
-  if test (shuf -n1 -e 0 1) -eq 0
-    set -l QUOTE_FILE "$HOME/.local/share/quotes/chess.quotes"	
-    set -l QUOTE (head -n (math (random) "%" (cat $QUOTE_FILE | wc -l) "+1") $QUOTE_FILE | tail -1)
-    eval (shuf -n1 -e cowsay cowthink) -(shuf -n1 -e b d g p s t w y) "$QUOTE"
-  else
-    fish_logo
+  switch (shuf -n1 -e (seq 1 4))
+    case 1
+      set -l QUOTE_FILE "$HOME/.local/share/quotes/chess.quotes"	
+      set -l QUOTE (head -n (math (random) "%" (cat $QUOTE_FILE | wc -l) "+1") $QUOTE_FILE | tail -1)
+      eval (shuf -n1 -e cowsay cowthink) -(shuf -n1 -e b d g p s t w y) "$QUOTE"
+    case 2
+      fish_logo
+    case 3
+      archey3
+    case 4
+      neofetch
   end
 end
