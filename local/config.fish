d1e5b337592ce0692dfbf9938b338c193d73a4eb
Juan <00jarg@gmail.com>
2020-05-10 21:51:37 -0300

    Add less colouring for man page reading.

--- local/config.fish
+++ local/config.fish
@@ -1,3 +1,11 @@
+# Set LESS colors for nicely reading man pages
+set -xU LESS_TERMCAP_md (printf "\e[01;31m")
+set -xU LESS_TERMCAP_me (printf "\e[0m")
+set -xU LESS_TERMCAP_se (printf "\e[0m")
+set -xU LESS_TERMCAP_so (printf "\e[01;44;33m")
+set -xU LESS_TERMCAP_ue (printf "\e[0m")
+set -xU LESS_TERMCAP_us (printf "\e[01;32m")
+
 set -x PATH $HOME/.local/bin $PATH
 set -x MPD_HOST $HOME/.config/mpd/socket
 if status --is-interactive
