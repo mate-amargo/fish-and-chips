d938633fc9b78f1ba555ac083e2e4f7a8158238c
Juan <00jarg@gmail.com>
2020-06-09 23:41:14 -0300

    Add cow variants, archey3, pfetch and doge

--- local/functions/fish_greeting.fish
+++ local/functions/fish_greeting.fish
@@ -1,14 +1,17 @@
-function fish_greeting -d "Sage cow displaying a quote at random"
-  switch (shuf -n1 -e (seq 1 4))
+function fish_greeting -d "Sage cow chess quote OR Fish OR Info OR Doge"
+  switch (shuf -n1 -e (seq 1 5))
     case 1
-      set -l QUOTE_FILE "$HOME/.local/share/quotes/chess.quotes"	
+      set -l QUOTE_FILE "$HOME/.local/share/quotes/chess.quotes"
       set -l QUOTE (head -n (math (random) "%" (cat $QUOTE_FILE | wc -l) "+1") $QUOTE_FILE | tail -1)
-      eval (shuf -n1 -e cowsay cowthink) -(shuf -n1 -e b d g p s t w y) "$QUOTE"
+      set -l COW (shuf -n1 -e blowfish default dragon dragon-and-cow eyes ghostbusters moose sheep skeleton small stegosaurus turkey turtle tux)
+      eval (shuf -n1 -e cowsay cowthink) -f $COW -(shuf -n1 -e b d g p s t w y) "$QUOTE" | lolcat
     case 2
       fish_logo
     case 3
       archey3
     case 4
-      neofetch
+      pfetch
+    case 5
+      doge
   end
 end
