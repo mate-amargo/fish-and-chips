f10a3174c25f8d64b48655fd772db863c5fe84fa
Juan <00jarg@gmail.com>
2020-06-09 23:40:41 -0300

    Use vim mode now that I use st and vi cursor works there

--- local/config.fish
+++ local/config.fish
@@ -6,8 +6,34 @@ set -xU LESS_TERMCAP_so (printf "\e[01;44;33m")
 set -xU LESS_TERMCAP_ue (printf "\e[0m")
 set -xU LESS_TERMCAP_us (printf "\e[01;32m")
 
+set -xU EDITOR vim
+
 set -x PATH $HOME/.local/bin $PATH
 set -x MPD_HOST $HOME/.config/mpd/socket
 if status --is-interactive
-  abbr --add --global cdd "cd ~/downloads"
+	abbr --add --global cdd "cd ~/downloads"
+	abbr --add --global l "exa -l --icons"
+end
+
+set fish_cursor_default     block      blink
+set fish_cursor_insert      line       blink
+set fish_cursor_replace_one underscore blink
+set fish_cursor_visual      block
+fish_hybrid_key_bindings
+
+# Fix del key in st
+switch $TERM
+case 'st-*' # suckless' simple terminal
+						# Enable keypad, do it once before fish_postexec ever fires
+	tput smkx
+	function st_smkx --on-event fish_postexec
+		tput smkx
+	end
+	function st_rmkx --on-event fish_preexec
+		tput rmkx
+	end
+end
+
+if test (tty | cut -d'/' -f3 | sed 's/[0-9]//g') = 'tty'
+	pgrep X || x
 end
